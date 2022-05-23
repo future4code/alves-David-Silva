a8c5f46da9d91eb7b551b86d50811edc37936558
David Henrique Souza da Silva <contatodavidhenrique@gmail.com>
2022-05-23 12:34:02 -0300

    exercicio 11

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -91,13 +91,12 @@ function retornaSegundoMaiorESegundoMenor(array) {
     segundoMaior = array[array.length-2]
     segundoMenor = array[1]
     newArr = [segundoMaior,segundoMenor]
-    return newArr
-    
+    return newArr    
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-
+ return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores.join(', ')}.`
 }
 
 // EXERCÍCIO 12
