39d058f464e4768db0c635605941cd4e6faf117e
David Henrique Souza da Silva <contatodavidhenrique@gmail.com>
2022-06-11 19:16:02 -0300

    continuando o projeto

--- modulo2/projeto-whatslab/src/App.js
+++ modulo2/projeto-whatslab/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import Chat from "./components/Chat";
+
+const MainContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+width: 100vw;
+height: 100vh;
+border: 5px solid greenyellow;
+background-color: gray;
+box-sizing: border-box;
+`
+const Mvp = styled.div`
+display: flex;
+justify-content: flex-end;
+align-items: flex-end;
+width: 50vw;
+height: 80vh;
+border: 1px solid black;
+box-sizing: border-box;
+background-color: greenyellow;
+
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer>
+      <Mvp>
+      <Chat />
+      </Mvp>
+      </MainContainer>
   );
 }
 
